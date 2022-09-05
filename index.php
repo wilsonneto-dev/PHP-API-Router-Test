4af3346cd27c627522bdb36dcdc30c698c6d502b
Wilson Neto B.R <contato@wilsonneto.com.br>
2022-09-05 15:53:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 require_once "./simple-router/Autoload.php";
 require_once "./modules/classes/api/classes-routes-register.php";
 
-$classesApiRouter = new Router();
-ClassesRoutesRegister::register($classesApiRouter);
+$router = new Router();
+ClassesRoutesRegister::register($router);
 
-$classesApiRouter->route($_GET["path"]);
+$router->route($_GET["path"]);
 
 ?>
